b3d310ba1512869b237807269e8e3ff30241484b
BTC950 <54998008+BTC950@users.noreply.github.com>
2020-10-01 17:02:46 -0500

    Update Restaurants.jsx
    just copied from the movies.

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import Page from '../containers/Page';
+import Page from '../containers/Page'; 
+/* this would get replaced with a call from to a database */
+import inception from '../assets/inception.jpeg' 
 
 const Restaurants = () => {
+  
+  // mock of API return
+  const cards = [
+    { title: 'Inception', categories: ['Thriller', 'Action'], platform: 'Netflix', runtime: '2h5m', image: inception, description: 'Mind Fuckery' },
+    /* tried to add another lot of photos to be used need to learn to intergrate a database and use that instead of this saved photos */ 
+  ];
+  
   return (
-    <Page title="Restaurants">
-      Restaurants content here
+    <Page title="Restaurants" contentTitle="Find Resturant">
+      <Deck cards={cards} />
     </Page>
   );
 };
